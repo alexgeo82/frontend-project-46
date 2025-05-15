bc05fea30ed754a82fe9b3ce8cf7f2432c1eb1fd
Александр Елякин <elyakinan@gmail.com>
2025-05-15 08:25:13 +0300

    Step 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/alexgeo82/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/alexgeo82/frontend-project-46/actions)
+
+[Step 4](https://asciinema.org/a/05CF8fOxQSPqHGvfmh0MZl9h1)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import parser from './parser.js';
 import { getFormat, getData } from './getdata.js';
+import diff from './jsondiff.js';
 
 export default (filepath1, filepath2) => {
   const data1 = parser(getData(filepath1), getFormat(filepath1));
   const data2 = parser(getData(filepath2), getFormat(filepath2));
-  console.log('differences');
+  const differences = diff(data1, data2);
+  return differences;
 };
