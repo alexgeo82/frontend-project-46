4337c6465bbb09945e235dadaca7cce0bf012ac0
Александр Елякин <elyakinan@gmail.com>
2025-05-19 08:42:19 +0300

    Step 5

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/alexgeo82/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/alexgeo82/frontend-project-46/actions)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=alexgeo82_frontend-project-46&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=alexgeo82_frontend-project-46)
+![Coverage](https://sonarcloud.io/api/project_badges/measure?project=alexgeo82_frontend-project-46&metric=coverage)
 
 [Step 4](https://asciinema.org/a/05CF8fOxQSPqHGvfmh0MZl9h1)
---
+++ __tests__/jsondiff.test.js
@@ -0,0 +1,24 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const expected = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('gendiff', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  const diff = gendiff(file1, file2);
+  expect(diff).toEqual(expected);
+
+})
